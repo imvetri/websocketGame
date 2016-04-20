772bf10ed3b2a4ec5fa5db3b8af74e83179f4111
imvetri <svetrivel.91@gmail.com>
2016-04-21 00:01:52 +0530

    added high level structure to javascript

--- gameStartsHere.js
+++ gameStartsHere.js
@@ -1,8 +1,45 @@
 (function(){
 	"use strict";
-
-	var gameInits = function(){
-		console.log('game started');
+	//this is bad. each time you access properity DOM will be accessed
+	var DOM = {
+		playerRank : document.getElementById('playerRank'),
+		playerJoystick : document.getElementById('playerJoystick')
 	};
-	window.addEventListener("load" , gameInits);
+
+	var player = {
+		score : 0,
+		init : function(){
+			DOM.playerJoystick.addEventListener('click' , player.increaseScore);
+		},
+		initScore : function(){
+			//if player already exists, update the score 
+			// else do nothing
+		},
+		increaseScore : function(){
+		//if game connection succeded, send currentScore+1 
+		}
+	};
+
+	var connection = {
+		init : function(){
+			//initiate websockt connection here
+		},
+		sendScore : function(){
+
+		},
+		close : function(){
+			//before window close, bind this function
+		},
+		onError : function(){
+			//on connection failure try reconnect
+			//on connection failure keep listening to score. this will provide offline scoring as well
+			//on reconnection update the score
+		},
+		reconnect : function(){
+
+		}
+	};
+
+	window.addEventListener("load" , player.init);
+	window.addEventListener("load" , connection.inits)
 })()

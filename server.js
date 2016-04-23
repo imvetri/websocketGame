aef8b00f15477ed92733e7c6ea44c1deab639615
imvetri <svetrivel.91@gmail.com>
2016-04-23 23:23:30 +0530

    restored server file

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+//dependencies
 var http = require('http');
+//functions
+var connectionCallback = function( request , response ){
+  console.log('request recieved');
+}
 
+
+http.createServer( connectionCallback ).listen(8080);
+
+/* following explains in detail how server starts.
+
+// server is an eventEmitter object
+// it listens to connection event
 var server = http.createServer(),
     requestHandler = function( request , response ) {
 
@@ -7,8 +19,8 @@ var server = http.createServer(),
         console.log('response '+response);
 
     };
-
+// here connection event is the request event
 server
     .listen('8080')
     .on('request' , requestHandler);
-
+*/

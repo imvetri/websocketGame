ca938a92d7dd548b98b37d08a90cb523c5adde3f
imvetri <svetrivel.91@gmail.com>
2016-04-25 00:04:32 +0530

    added details to server eventemiter alternative solutions

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ var connectionCallback = function( request , response ){
   console.log('request recieved');
 }
 
-
+//Refer to #1 for under-the-hood working of server listen
 http.createServer( connectionCallback ).listen(8080);
 //working and add websockets here
 
-/* following explains in detail how server starts.
+/* #1 server
+// server object is an eventEmitter object
+// Eventemitters are capable of emiting an event and any event listeners linked to this event will execute a callback
+// in case of server-eventEmitter it listens to connection event
+// this includes request and response  as both of these mean as a conenction
 
-// server is an eventEmitter object
-// it listens to connection event
+Todo:
+following code is tested for request as a connection object and everything seems to be working fine .
+need to test the same for response as a connections
+
 var server = http.createServer(),
     requestHandler = function( request , response ) {
 

584afc5785690c1d588f13ee6406e6af17e47a40
imvetri <svetrivel.91@gmail.com>
2016-04-26 21:52:50 +0530

    final server file without websocket

--- server.js
+++ server.js
@@ -1,40 +1,28 @@
 //dependencies
-var http = require('http');
+var http = require('http'),
+    ws = require('websocket').server;
+
+
 //functions
 var connectionCallback = function( request , response ){
-  console.log('request recieved');
-}
-
-//Refer to #1 for under-the-hood working of server listen
-http.createServer( connectionCallback ).listen(8080);
-//working and add websockets here
-
-/* #1 server
-// server object is an eventEmitter object
-// Eventemitters are capable of emiting an event and any event listeners linked to this event will execute a callback
-// in case of server-eventEmitter it listens to connection event
-// this includes request and response  as both of these mean as a conenction
-
-Todo:
-following code is tested for request as a connection object and everything seems to be working fine .
-need to test the same for response as a connections
-Result:
-tried that. but the connection event didnt trigger any event on response to a connection. you can try this by uncommenting
-lines 25 - 39
-
-var server = http.createServer(),
-    requestHandler = function( request , response ) {
-
-        console.log('request '+request);
-        response.writeHead(200);
-        response.end("over");
+        console.log('Request received at the server');
+        response.writeHead('200');
+        response.end();
     },
-    responseHandler = function(a,b){
-        console.log('response');
-    }
-// here connection event is the request event
-server
-    .listen('8080')
-    .on('request' , requestHandler)
-    .on('response' , responseHandler);
-*/
+    isOriginAllowed = function( origin ){
+        return true;
+    };
+
+//variables
+var server = http.createServer( connectionCallback ),
+    wsServer = new ws({
+        httpServer : server,
+        autoAcceptConnections : false
+    }),
+    serverPort = '8080';
+
+//executions
+server.listen(serverPort , ()=> console.log('Server Listening at port '+serverPort)); //Refer to #1 for under-the-hood working of server listen
+
+
+//working and add websockets here

cbb8ed0309055d59736ac0c9c749b8793d66841b
imvetri <svetrivel.91@gmail.com>
2016-04-26 22:26:36 +0530

    websocket implementation done

--- server.js
+++ server.js
@@ -4,25 +4,58 @@ var http = require('http'),
 
 
 //functions
-var connectionCallback = function( request , response ){
+var serverConnectionCallback = function( request , response ){
         console.log('Request received at the server');
         response.writeHead('200');
         response.end();
+    };
+
+//server variables
+var server = http.createServer( serverConnectionCallback ) ,
+    serverPort = '8080';
+
+
+//websockets variables
+var wsServer = new ws({
+        httpServer : server,
+        autoAcceptConnections : false
+    });
+
+//websocket functions
+var onMessageCallback = function( message ){
+        if( message.type === 'utf8' ) {
+            console.log( 'Message recieved - ' + message.utf8Data );
+            connection.sendUTF( message.utf8Data );
+        }
+        else if( message.type === 'binary' ){
+            console.log( 'Message received lenght of ' + message.binaryData.length );
+            connection.sendBytes( message.binaryData );
+        }
+    },
+    onCloseCallback = function( reasonCode , description ){
+        console.log( 'Disconnected ');
+    },
+    websocketCallback = function( request ){
+
+        if( !isOriginAllowed( request.origin ) ){
+            request.reject();
+            return;
+        }
+
+        var connection = request.accept( 'echo-protocol' , request.origin );
+        console.log('Request Accepted');
+
+        connection.on( 'message' , onMessageCallback );
+        connection.on( 'close' , onCloseCallback )
     },
     isOriginAllowed = function( origin ){
         return true;
     };
 
-//variables
-var server = http.createServer( connectionCallback ),
-    wsServer = new ws({
-        httpServer : server,
-        autoAcceptConnections : false
-    }),
-    serverPort = '8080';
 
-//executions
+//server executions
 server.listen(serverPort , ()=> console.log('Server Listening at port '+serverPort)); //Refer to #1 for under-the-hood working of server listen
 
 
-//working and add websockets here
+//webserver executions
+wsServer.on( ' request' , websocketCallback );

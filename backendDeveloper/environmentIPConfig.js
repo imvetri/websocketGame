5ed970b8477b3e01bfa2c0072da101490fcc6f12
imvetri <svetrivel.91@gmail.com>
2016-05-15 22:59:02 +0530

    fixed ws port at the front end

--- backendDeveloper/environmentIPConfig.js
+++ backendDeveloper/environmentIPConfig.js
@@ -4,7 +4,7 @@
 
 
 var _ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var _port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var _port      = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
 module.exports.ipaddress = _ipaddress;
 module.exports.port = _port;
